6fbed8b69dc42457fbdca4809984bc54cf562e17
Antonio Tirone <tironeantonio@gmail.com>
2021-12-02 16:24:44 +0100

    day 2: 2 stars!

--- day2/step2.py
+++ day2/step2.py
@@ -2,12 +2,14 @@ with open("input.txt") as f_in:
     lines_split = (s.split() for s in f_in.readlines())
     x_pos = 0
     y_pos = 0
+    aim = 0
     for line in lines_split:
         if line[0] == "forward":
             x_pos += int(line[1])
+            y_pos += int(line[1]) * aim
         elif line[0] == "down":
-            y_pos += int(line[1])
+            aim += int(line[1])
         else:
-            y_pos -= int(line[1])
+            aim -= int(line[1])
 
     print(x_pos * y_pos)
