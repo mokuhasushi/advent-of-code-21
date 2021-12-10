e1c42e6e9fcccb16569f1d069abee02129cbe28d
Antonio Tirone <tironeantonio@gmail.com>
2021-12-10 11:30:44 +0100

    day 10 part 2

--- day10/step2.py
+++ day10/step2.py
@@ -5,5 +5,40 @@ if len(sys.argv) > 1:
     if sys.argv[1] == "test":
         f_in_name = "sample.txt"
 
+
+def score(c):
+    if c == ")":
+        return 1
+    if c == "]":
+        return 2
+    if c == "}":
+        return 3
+    if c == ">":
+        return 4
+
+
 with open(f_in_name) as f_in:
-    pass
+    scores = []
+    closing = {"(": ")", "[": "]", "{": "}", "<": ">"}
+    for line in f_in.readlines():
+        line = line.strip()
+        control = []
+        line_score = 0
+        incomplete = True
+        for c in line:
+            if c in {"(", "{", "[", "<"}:
+                control.append(closing[c])
+            else:
+                ex = control.pop()
+                if c != ex:
+                    incomplete = False
+                    break
+        if not incomplete:
+            continue
+        control.reverse()
+        for n in control:
+            line_score *= 5
+            line_score += score(n)
+        scores.append(line_score)
+    s_scores = sorted(scores)
+    print(s_scores[len(s_scores) // 2])
